d828334f5324f257eface60aee1cbb89de96464d
Nataliia Svystula <natashasvistula@gmail.com>
2023-02-07 15:38:59 +0200

    add

---
+++ contacts.js
@@ -0,0 +1,26 @@
+const fs = require("fs").promises;
+const path = require("path");
+require("colors");
+const { nanoid } = require("nanoid");
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
