a3cceb670b592b9c126db5828b7f660389aa77d0
Nataliia Svystula <natashasvistula@gmail.com>
2023-02-08 21:08:23 +0200

    add

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# nodeJS-hw1
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+node index.js --action list
+https://monosnap.com/file/pQTEfwQY2L6UFPqMOlI3Pyis5NHfpF
+
+# Получаем контакт по id
+node index.js --action get --id 5
+https://monosnap.com/file/X44jLSOqf7Cnk3YKlIoHYh0YB0wkDa
+
+# Добавялем контакт
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/ja5QH0pUOm8xZ7lzKpL3KURr9anxCX
+
+# Удаляем контакт
+node index.js --action remove --id=3
+https://monosnap.com/file/ET6L2dMGQMWxHa82hHQCSZW1Js1Ue9
+
--- contacts.js
+++ contacts.js
@@ -1,30 +1,68 @@
-const fs = require("fs").promises;
-const path = require("path");
-require("colors");
-const { nanoid } = require("nanoid");
+const fs = require('fs').promises;
+const path = require('path');
+require('colors');
+const { randomUUID } = require("crypto");
 
 
-
-const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+const contactsPath = path.join(__dirname, './db/contacts.json');
   
   // TODO: задокументировать каждую функцию
   const  listContacts = async () => {
  try {
-		const contacts = await fs.readFile(contactsPath, { encoding: 'utf-8' })
-		return JSON.parse(contacts)
+   const contacts = await fs.readFile(contactsPath, { encoding: 'utf-8' })
+   const result = JSON.parse(contacts);
+  //  console.log(result)
+		return result;
 	} catch (error) {
 		console.log(`Error: ${error.message}`.red)
 	}
   };
 
-function getContactById(contactId) {
-  // ...твой код
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts()
+    const getContact = contacts.filter(({ id }) => id === contactId);
+		return getContact;
+	} catch (error) {
+		console.log(`Error: ${error.message}`.red)
+	}
 }
 
-function removeContact(contactId) {
-  // ...твой код
+const removeContact = async(contactId) => {
+  try {
+		const contacts = await listContacts()
+		const newContacts = contacts.filter(({ id }) => id !== contactId)
+		await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2), { encoding: 'utf-8' })
+
+		return newContacts
+	} catch (error) {
+		console.log(`Error: ${error.message}`.red)
+	}
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+const addContact = async (name, email, phone) => {
+  try {
+        
+		const contacts = await listContacts()
+		const newContact = {
+      id: randomUUID,
+      name,
+      email,
+      phone,
+    };
+		const updatedContacts = [newContact, ...contacts]
+		await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2), { encoding: 'utf-8' })
+		return newContact
+	} catch (error) {
+		console.log(`Error: ${error.message}`.red)
+	}
 }
+
+listContacts();
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
